edbf431fc594a18548a15b11e07075343e75b62d
nalawadepiyush <nalawadepiyush@gmail.com>
2019-01-03 06:40:57 +0530

    Teacher view separated

--- school_management_app/urls.py
+++ school_management_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from school_management_app import teacher_views
 from . import views
 
 urlpatterns = [
@@ -9,12 +10,12 @@ urlpatterns = [
     path('user/home/', views.render_homepage, name='render_homepage'),
     path('user/logout/', views.user_logout, name='user_logout'),
     path('teacher/enroll-student/', views.enroll_student, name='enroll_students_to_course-dashboard'),
-    path('teacher/student-engagement/', views.engage_student, name='engage_students'),
-    path('teacher/exams/', views.create_exams, name='create-exams-dashboard'),
-    path('teacher/exam/add/', views.insert_exam, name='create-exams'),
-    path('teacher/active-exams/', views.close_active_exams_dashboard, name='close-active-exam-dashboard'),
-    path('teacher/exam/finish/', views.close_active_exams, name='close-active-exam'),
-    path('teacher/exam/marks/', views.assign_exam_marks_dashboard, name='assign-marks'),
-    path('teacher/exam/assign-marks/', views.assign_exam_marks, name='assign-exam-marks'),
+    path('teacher/student-engagement/', teacher_views.engage_student, name='engage_students'),
+    path('teacher/exams/', teacher_views.create_exams, name='create-exams-dashboard'),
+    path('teacher/exam/add/', teacher_views.insert_exam, name='create-exams'),
+    path('teacher/active-exams/', teacher_views.close_active_exams_dashboard, name='close-active-exam-dashboard'),
+    path('teacher/exam/finish/', teacher_views.close_active_exams, name='close-active-exam'),
+    path('teacher/exam/marks/', teacher_views.assign_exam_marks_dashboard, name='assign-marks'),
+    path('teacher/exam/assign-marks/', teacher_views.assign_exam_marks, name='assign-exam-marks'),
 
 ]
