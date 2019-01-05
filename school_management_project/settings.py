b0489381201ccf1091e0dcc85356436b437b9245
nalawadepiyush <nalawadepiyush@gmail.com>
2019-01-05 20:47:09 +0530

    Heroku config stsatic files

--- school_management_project/settings.py
+++ school_management_project/settings.py
@@ -118,8 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -133,6 +131,14 @@ JWT_COOKIE_EXPIRATION = datetime.timedelta(days=364)
 
 APPEND_SLASH = False
 
-import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
