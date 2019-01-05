12ef4f2d455bb7b948bdfbe2060836b7a3658861
nalawadepiyush <nalawadepiyush@gmail.com>
2019-01-06 01:12:00 +0530

    Changes for production

--- school_management_project/settings.py
+++ school_management_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '34v_lsk^9t8_22retnpxgl0ks-v)^01ky6q*t1vtq&%j$!woko'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
