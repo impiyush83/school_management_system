43e2375d67ef4ebe1d3f11c3ddb83a1f7879cab6
nalawadepiyush <nalawadepiyush@gmail.com>
2019-01-05 23:43:36 +0530

    Settings.py changed

--- school_management_project/settings.py
+++ school_management_project/settings.py
@@ -137,9 +137,3 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
