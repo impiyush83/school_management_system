3c69195f055bf84137d828c632734efcf86cfa25
nalawadepiyush <nalawadepiyush@gmail.com>
2019-01-06 00:29:30 +0530

    Whitenose added to serve staticfiles

--- school_management_project/settings.py
+++ school_management_project/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'school_management_project.urls'
